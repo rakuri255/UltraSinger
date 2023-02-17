ee72dd3dd7789b1628e347754308befc162d935d
Vadim <rakuri255@yahoo.de>
2023-02-17 22:02:11 +0100

    optimized hyphenation

--- moduls/Speech_Recognition/hyphenation.py
+++ moduls/Speech_Recognition/hyphenation.py
@@ -1,11 +1,41 @@
-import pyphen
+from hyphen import Hyphenator
+from hyphen import dictools
+import string
 
 
-def hyphenation(word, language='en'):
-    dic = pyphen.Pyphen(lang=language)
+def language_check(language='en'):
+    dict_langs = dictools.LANGUAGES
 
-    hyphen = None
-    for pair in dic.iterate(word):
-        hyphen = pair  # Todo: ignore the others?
+    lang_region = None
+    for dict_lang in dict_langs:
+        if dict_lang.startswith(language):
+            lang_region = dict_lang
+            break
+
+    if lang_region is None:
+        raise Exception('Missing language "{}" for hyphen. Install it or disable hyphen.'.format(language))
+
+    print("Hyphenate using language code: {}".format(lang_region))
+    return lang_region
+
+
+def contains_punctuation(word):
+    return any(elem in word for elem in string.punctuation)
+
+
+def hyphenation(word, lang_region):
+    if contains_punctuation(word):
+        return None
+
+    hy = Hyphenator(lang_region)
+    syllabus = hy.syllables(word)
+
+    length = len(syllabus)
+    if length > 1:
+        hyphen = []
+        for i in range(length):
+            hyphen.append(syllabus[i])
+    else:
+        hyphen = None
 
     return hyphen
