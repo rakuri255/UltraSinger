13477dde06214a68f2e3c3f5e71cd2298af4a458
Vadim <rakuri255@yahoo.de>
2023-05-21 22:25:47 +0200

    - fix cpu argument for separation

--- moduls/Audio/separation.py
+++ moduls/Audio/separation.py
@@ -8,7 +8,7 @@ def separate_audio(input_file_path, output_file, device="cpu"):
     # Model selection?
     # -n mdx_q
     # -n htdemucs_ft
-    device = "-d cpu" if device == "cpu" else "cuda"
+    device = "cpu" if device != "cuda" else "cuda"
     subprocess.run(["demucs", "-d", device, "--two-stems=vocals", input_file_path])
 
     separated_folder = path_join(current_executor_path(), "separated")
