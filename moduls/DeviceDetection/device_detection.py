dcd18cab7919a6ed5e7823f8c8ee44bba15a8bdd
Vadim <rakuri255@yahoo.de>
2023-05-29 13:55:56 +0200

    - Added f strings

--- moduls/DeviceDetection/device_detection.py
+++ moduls/DeviceDetection/device_detection.py
@@ -5,9 +5,9 @@ def get_available_device():
     isCuda = torch.cuda.is_available()
 
     if not isCuda:
-        print("{} There are no {} devices available. -> Using {}.".format(PRINT_ULTRASTAR, print_red_highlighted_text("cuda"), print_red_highlighted_text("cpu")))
+        print(f"{PRINT_ULTRASTAR} There are no {print_red_highlighted_text('cuda')} devices available. -> Using {print_red_highlighted_text('cpu')}.")
         return "cpu"
     else:
-        print("{} Using {} GPU.".format(PRINT_ULTRASTAR, print_red_highlighted_text("cuda")))
+        print(f"{PRINT_ULTRASTAR} Using {print_red_highlighted_text('cuda')} GPU.")
 
     return "cuda"
