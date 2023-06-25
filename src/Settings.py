a42ebce4b79465471e00244914fd80c689ed3097
Vadim <rakuri255@yahoo.de>
2023-06-26 01:30:06 +0200

    - drop vosk support

--- src/Settings.py
+++ src/Settings.py
@@ -16,15 +16,12 @@ class Settings:
     audio_chunk_folder_name = "audio-chunks"
 
     # Whisper
-    transcriber = "whisper"  # whisper|vosk
+    transcriber = "whisper"  # whisper
     whisper_model = "large-v2"  # Multilingual model tiny|base|small|medium|large-v1|large-v2
     # English-only model tiny.en|base.en|small.en|medium.en
     whisper_align_model = None   # Model for other languages from huggingface.co e.g -> "gigant/romanian-wav2vec2"
     whisper_batch_size = 16   # reduce if low on GPU mem
     whisper_compute_type = None   # change to "int8" if low on GPU mem (may reduce accuracy)
-
-    # Vosk
-    vosk_model_path = ""  # "models/vosk-model-small-en-us-0.15"
 
     # Pitch
     crepe_model_capacity = "full"  # tiny|small|medium|large|full
