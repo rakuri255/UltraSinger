b52945be200d649d992678f12ede9ff4a490ddf3
Benedikt Wagener <wagenerbene@gmail.com>
2023-10-03 07:52:01 +0200

    wip

--- src/modules/plot.py
+++ src/modules/plot.py
@@ -175,7 +175,7 @@ def determine_bounds(frequency_log_10: list[float]) -> tuple[float, float]:
 def set_figure_dimensions(time_range, frequency_log_10_range):
     """Dynamically scale the figure dimensions based on the duration/frequency amplitude of the song"""
     height = frequency_log_10_range / 0.06
-    width = time_range / 2
+    width = time_range / 4
 
     plt.figure(1).set_figwidth(max(6.4, width))
     plt.figure(1).set_figheight(max(4, height))
@@ -212,7 +212,7 @@ def create_gaps(pitched_data: PitchedData, step_size: float) -> PitchedData:
 
 def draw_words(transcribed_data, midi_notes):
     """Draw rectangles for each word"""
-    if transcribed_data is not None:
+    if transcribed_data is not None and len(transcribed_data) > 0:
         for i, data in enumerate(transcribed_data):
             note_frequency = librosa.note_to_hz(midi_notes[i])
             frequency_range = get_frequency_range(midi_notes[i])
