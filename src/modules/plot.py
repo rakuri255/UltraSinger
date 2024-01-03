564ac26c2e87d4d2d9fcbb27853a010eed683e25
Vadim <rakuri255@yahoo.de>
2024-01-03 15:55:08 +0100

    Added words to Plot

--- src/modules/plot.py
+++ src/modules/plot.py
@@ -236,6 +236,7 @@ def draw_words(transcribed_data, midi_notes):
                 alpha=0.5,
             )
             plt.gca().add_patch(rect)
+            plt.text(data.start + width/4, numpy.log10([note_frequency + half_frequency_range])[0], data.word, rotation=90)
 
 
 def snake(s):
