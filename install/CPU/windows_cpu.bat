d63b72b24746f65ea06729cbb93f1cef7cd793a6
Vadim <rakuri255@yahoo.de>
2024-10-19 22:47:00 +0200

    Fix CPU installer - add missing tourch

--- install/CPU/windows_cpu.bat
+++ install/CPU/windows_cpu.bat
@@ -1,6 +1,7 @@
 @echo off
 cd ..
 cd ..
-py -3.10 -m venv .venv
+py -3.10 -m venv .venv2
 call .venv\Scripts\activate
 pip install -r requirements.txt
+pip install torch==2.0.1 torchvision==0.15.2 torchaudio==2.0.2
