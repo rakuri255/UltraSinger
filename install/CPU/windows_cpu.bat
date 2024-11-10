859af30a1652a33f28ed55070e5c545bdbf80bb9
Vadim Rangnau <36919102+rakuri255@users.noreply.github.com>
2024-11-10 22:23:34 +0100

    Update windows_cpu.bat
    move venv2 to venv

--- install/CPU/windows_cpu.bat
+++ install/CPU/windows_cpu.bat
@@ -1,7 +1,7 @@
 @echo off
 cd ..
 cd ..
-py -3.10 -m venv .venv2
+py -3.10 -m venv .venv
 call .venv\Scripts\activate
 pip install -r requirements.txt
 pip install torch==2.0.1 torchvision==0.15.2 torchaudio==2.0.2
