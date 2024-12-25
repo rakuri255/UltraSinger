20499960c197190e02b793f3fe52b06f4422e68d
TheZet <TheZet@Home.com>
2024-12-25 09:18:58 -0500

    Added setlocal and endlocal

--- install/CPU/windows_cpu.bat
+++ install/CPU/windows_cpu.bat
@@ -1,7 +1,9 @@
 @echo off
+setlocal
 cd ..
 cd ..
 py -3.10 -m venv .venv
 SET VenvPythonPath=%CD%\.venv\Scripts\python.exe
 call %VenvPythonPath% -m pip install -r requirements.txt
 call %VenvPythonPath% -m pip install torch==2.0.1 torchvision==0.15.2 torchaudio==2.0.2
+endlocal
